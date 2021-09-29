cce8db2ee36779c8053f658a84f463a945577ff7
Michele Cozzola <cozzolamichele89@gmail.com>
2021-09-29 11:54:59 +0200

    play & pause

---
+++ script.js
@@ -0,0 +1,55 @@
+let tracks = [
+    {   url : './audio/Ghostrifter Official - Midnight Stroll.mp3' , 
+    cover : './cover/cover-3.jpg', artist : 'Ghostrifter' , title : 'Midnight Stroll'},
+    {   url : './audio/Ghostrifter Official - Hot Coffee.mp3' , 
+    cover : './cover/cover-1.jpg', artist : 'Ghostrifter' , title : 'Hot Coffee'},
+    {   url : './audio/Ghostrifter Official - Merry Bay.mp3' , 
+    cover : './cover/cover-2.jpg', artist : 'Ghostrifter' , title : 'Merry Bay'},
+    {   url : './audio/Ghostrifter Official - Morning Routine.mp3' , 
+    cover : './cover/cover-4.jpg', artist : 'Ghostrifter' , title : 'Morning Routine'},
+    {   url : './audio/Ghostrifter Official - Still Awake.mp3' , 
+    cover : './cover/cover-5.jpeg', artist : 'Ghostrifter' , title : 'Still Awake'},
+    {   url : './audio/Ghostrifter Official - Afternoon Nap.mp3' , 
+    cover : './cover/cover-6.jpg', artist : 'Ghostrifter' , title : 'Afternoon Nap'},
+    {   url : './audio/Ghostrifter Official - Demised To Shield.mp3' , 
+    cover : './cover/cover-7.jpg', artist : 'Ghostrifter' , title : 'Demised To Shield'},
+    {   url : './audio/Ghostrifter Official - Departure.mp3' , 
+    cover : './cover/cover-8.jpg', artist : 'Ghostrifter' , title : 'Departure'},
+    {   url : './audio/Ghostrifter Official - Mellow Out.mp3' , 
+    cover : './cover/cover-9.jpg', artist : 'Ghostrifter' , title : 'Mellow Out'},
+    {   url : './audio/Ghostrifter Official - On My Way.mp3' , 
+    cover : './cover/cover-10.jpg', artist : 'Ghostrifter' , title : 'On My Way'},
+    {   url : './audio/Ghostrifter Official - Simplicit Nights.wav' , 
+    cover : './cover/cover-1.jpg', artist : 'Ghostrifter' , title : 'Simplicit Nights'},
+    {   url : './audio/Ghostrifter Official - Soaring.mp3' , 
+    cover : './cover/cover-2.jpg', artist : 'Ghostrifter' , title : 'Soaring'},
+    {   url : './audio/Ghostrifter Official - Subtle Break.mp3' , 
+    cover : './cover/cover-3.jpg', artist : 'Ghostrifter' , title : 'Subtle Break'},
+    {   url : './audio/Ghostrifter Official - Transient.mp3' , 
+    cover : './cover/cover-4.jpg', artist : 'Ghostrifter' , title : 'Transient'},
+]
+
+/* variabile globale per far partire la traccia */
+let track = document.querySelector('#track')
+/* associazione dal dom */
+let playBtn = document.querySelector('#play-btn')
+let pauseBtn = document.querySelector('#pause-btn')
+let playing = false
+
+/* funzione per avviare il file audio */
+function play() {
+    if (!playing) {
+        playBtn.classList.add('d-none')
+        pauseBtn.classList.remove('d-none')
+        track.play()
+        playing = true        
+    }else{
+        playBtn.classList.remove('d-none')
+        pauseBtn.classList.add('d-none')
+        track.pause()
+        playing = false
+    }
+}
+
+playBtn.addEventListener('click', play)
+pauseBtn.addEventListener('click', play)
